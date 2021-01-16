bc051743b147c5b31915742b1aa3172a01f70d48
Waldis Jr <waldis_jr@outlook.com>
2021-01-16 09:37:40 +0200

    openapi

--- openapi_parser/main.py
+++ openapi_parser/main.py
@@ -1,26 +1,29 @@
-import requests
 from bs4 import BeautifulSoup
-from os import path
+import os
+import requests
+import yaml
 
-global main_url
 
-
-
-def get_main_file(request_url: str) -> str:
+def get_main_file_url(request_url: str) -> str:
     resp = requests.get(request_url).text
     soup = BeautifulSoup(resp, 'lxml')
     if soup.find('redoc') is not None and 'spec-url' in soup.find('redoc').attrs:
         spec_url = soup.find('redoc').attrs['spec-url']
-        global main_url = path.dirname(request_url + spec_url)
-        with open(path.basename(spec_url))
+        return request_url + spec_url
 
 
-def download_yaml_file(request_url: str) -> str:
+def download_and_parse_yaml_file(request_url: str) -> str:
+    if not os.path.exists(dir): 
+        os.mkdir(dir)
+    with open(f"/{spec_url}", 'w') as file:
+        file.write()
+    return os.path.dirname(full_url)
 
-    pass
+
+def main(main_url: str):
+    main_file_url = get_main_file_url(main_url)
+    work_url = os.path.dirname(main_file_url)
 
 
 if __name__ == '__main__':
-    path = 'http://develop.jazzserve.com:9095/innovate/doc/'
-    get_main_file(path)
-
+    main('http://develop.jazzserve.com:9095/innovate/doc/')
